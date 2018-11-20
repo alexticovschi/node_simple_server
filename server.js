253ba0d1c2614c1b875c970c05eb82421a25ccad
AlexTicovschi <alexmticovschi@gmail.com>
2018-11-20 17:29:33 +0000

    adding routing

--- server.js
+++ server.js
@@ -1,31 +1,40 @@
 const http = require('http');
 const fs = require('fs');
 
-let html = fs.readFileSync('./index.html');
+const server = http.createServer((req,res) => {
+    
+    if (req.url === "/") {
+        res.writeHead(200, { 'Content-type':'text/html' }); 
+        let html = fs.readFileSync('./index.html');
+        res.end(html)
+    } else if (req.url === "/api/user") {
+        res.writeHead(200, { 'Content-type':'application/json' }); 
 
-const server = http.createServer((req,res)=>{
-    res.writeHead(200, { 'Content-type':'application/json' });    
-    
-    const json = JSON.stringify({
-        "name": "mkyong",
-        "age": 30,
-        "address": {
-            "streetAddress": "88 8nd Street",
-            "city": "New York"
-        },
-        "phoneNumber": [
-            {
-                "type": "home",
-                "number": "111 111-1111"
+        const json = JSON.stringify({
+            "name": "Francis",
+            "age": 30,
+            "address": {
+                "streetAddress": "88 8nd Street",
+                "city": "New York"
             },
-            {
-                "type": "fax",
-                "number": "222 222-2222"
-            }
-        ]
-    });
-    
-    res.end(json.toString());
+            "phoneNumber": [
+                {
+                    "type": "home",
+                    "number": "111 111-1111"
+                },
+                {
+                    "type": "fax",
+                    "number": "222 222-2222"
+                }
+            ]
+        });
+        
+        res.end(json);
+    } else {
+        res.writeHead(404);
+        res.end();
+    }
+
 });
 
 server.listen( 8181, '127.0.0.1' );
