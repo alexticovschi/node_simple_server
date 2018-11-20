64be4ef3f057efc72efda4d2de5b884f40706edf
AlexTicovschi <alexmticovschi@gmail.com>
2018-11-20 14:53:19 +0000

    sending JSON data

--- server.js
+++ server.js
@@ -4,8 +4,28 @@ const fs = require('fs');
 let html = fs.readFileSync('./index.html');
 
 const server = http.createServer((req,res)=>{
-    res.writeHead(200, { 'Content-type':'text/html' });
-    res.end(html)
+    res.writeHead(200, { 'Content-type':'application/json' });    
+    
+    const json = JSON.stringify({
+        "name": "mkyong",
+        "age": 30,
+        "address": {
+            "streetAddress": "88 8nd Street",
+            "city": "New York"
+        },
+        "phoneNumber": [
+            {
+                "type": "home",
+                "number": "111 111-1111"
+            },
+            {
+                "type": "fax",
+                "number": "222 222-2222"
+            }
+        ]
+    });
+    
+    res.end(json.toString());
 });
 
 server.listen( 8181, '127.0.0.1' );
